5758a45a72ceeac05296afb48eb148c99bb6b435
Gorororo007 <isip_g.l.davtyan@mpt.ru>
2025-11-10 16:07:26 +0300

    Revert "практ2"

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AuthProvider } from './contexts/AuthContext'
-import { CartProvider } from './contexts/CartContext'
-import { ThemeProvider } from './contexts/ThemeContext'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-      <AuthProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </AuthProvider>
-    </ThemeProvider>
+    <App />
   </StrictMode>,
 )
